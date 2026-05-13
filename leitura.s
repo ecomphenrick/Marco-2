62003af2166b1bbb8da8a6ebc87eb70d47abaa02
Henrick Ferreira <henrickferreira77@hotmail.com>
2026-05-13 09:55:57 -0300

    feat: iniciando funções

--- leitura.s
+++ leitura.s
@@ -1,38 +1,47 @@
 .data
-caminho: .asciz "/home/oem/Downloads/pbl/b_q.bin" @caminho do arquivo
-buffer: .space 256
+caminho_bias: .asciz "/home/oem/Downloads/pbl/b_q.bin" @caminho do arquivo do bias
+caminho_beta: @colocar caminho
+buffer_bias: .space 256
+buffer_beta: .space 2560
 
 .text
 .global _start
 
 _start:
-	ldr r0, =caminho @ caminho do arquivo
-	mov r1, #0	 @ somente leitura
-	mov r2, #0	 @ modo
-	mov r7, #5	 @ chamada ao sistema (open)
-	swi 0		 @ chama o linux, fecha...
+	bl store_bias @chama a função
+	mov r7, #1 	@exit
+	swi 0 		@util para o echo
 	
-	mov r8, r0	 @ salva o file descriptor
+
+store_bias:
+
+	ldr r0, =caminho_bias @ caminho do arquivo
+        mov r1, #0       @ somente leitura
+        mov r2, #0       @ modo
+        mov r7, #5       @ chamada ao sistema (open)
+        swi 0            @ chama o linux, fecha...
+
+        mov r8, r0       @ salva o file descriptor
 
 
-	mov r0, r8       @ fd
-	ldr r1, =buffer  @ endereço do buffer
-	mov r2, #256	 @qtd de bytes
-	mov r7, #3	 @ chamada ao sistema read
-	swi 0	 	 @ chama o linux e fecha
+        mov r0, r8       @ fd
+        ldr r1, =buffer_bias  @ endereço do buffer
+        mov r2, #256     @qtd de bytes
+        mov r7, #3       @ chamada ao sistema read
+        swi 0            @ chama o linux e fecha
+
+        mov r9, r0      @ salva quantos bytes foram lidos
+
+        mov r0, r8      @fd
+        mov r7, #6      @ chamada ao sistema de fechamento
+        swi 0           @ chama o linx
+
+        ldr r0, =buffer_bias @endereço do buffer
+        ldrh r1, [r0]  @le os 2 primeiros bytes
 	
-	mov r9, r0	@ salva quantos bytes foram lidos
+	mov r0, r1 @retornando valor
+
+        bx lr
+
+store_beta:
 	
-	mov r0, r8 	@fd
-	mov r7, #6	@ chamada ao sistema de fechamento
-	swi 0		@ chama o linx
-
-	ldr r0, =buffer	@endereço do buffer
-	ldrh r1, [r0]  @le os 2 primeiros bytes
-
-	mov r7, #1	@exit
-	mov r0, r1	@sai com o numero de bytes lidos
-	swi 0
-
-
-
