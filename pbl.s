09d56761b7bab5e6dd57c353508626a5b9b1a491
Henrick Ferreira <henrickferreira77@hotmail.com>
2026-05-15 13:18:41 -0300

    feat: comentando código assembly

--- pbl.s
+++ pbl.s
@@ -1,44 +1,41 @@
 .data
-caminho_bias: .asciz "/home/oem/Downloads/pbl/b_q.bin"
-buffer_bias:  .space 256
+caminho_bias: .asciz "/home/oem/Downloads/pbl/b_q.bin" @caminho do arquivo bias
+buffer_bias:  .space 256 @espaço destinado ao buffer (128x2 bytes)
 
 .text
 .global _start
 
 _start:
-    bl   store_bias
+    bl   store_bias @chamada da função
 
-    mov  r7, #1
+    mov  r7, #1 
     swi  0
 
-@ ============================================================
-@ store_bias: lê buffer e monta instrução
-@ ============================================================
 store_bias:
-    push {r7, r8, r9, lr}
+    push {r8, r9}
 
-    ldr  r0, =caminho_bias
-    mov  r1, #0
-    mov  r2, #0
-    mov  r7, #5
-    swi  0
-    mov  r8, r0
+    ldr  r0, =caminho_bias @r0 endereço da string
+    mov  r1, #0 @somente leitura
+    mov  r2, #0 @nao usado
+    mov  r7, #5 @syscall open
+    swi  0   #chama o linux com os parametros
+    mov  r8, r0 @salva em r8 o endereço da string (para nao perder)
 
-    mov  r0, r8
-    ldr  r1, =buffer_bias
-    mov  r2, #256
-    mov  r7, #3
-    swi  0
-    mov  r9, r0
+    mov  r0, r8 (devolve o valor a r0, agora é o fd = arquivo 3 por exempo)
+    ldr  r1, =buffer_bias @r1 = endereço do bias
+    mov  r2, #256 @tamanho do buffer
+    mov  r7, #3 @modo de leitura
+    swi  0  @ chama o linux
+    mov  r9, r0 @r9 pega oq devolveu (256bytes)
 
-    mov  r0, r8
-    mov  r7, #6
-    swi  0
+    mov  r0, r8 @fd novamente em r0
+    mov  r7, #6 @syscall de fechamento
+    swi  0 @fecha o arquivo e retorna em r0
 
-    ldr  r0, =buffer_bias
-    ldrh r1, [r0]
-    rev16 r1, r1
-    sxth  r1, r1
+    ldr  r0, =buffer_bias @r0 aponta para endereço do buffer
+    ldrh r1, [r0] @le 2 bytes
+    rev16 r1, r1 @reverte (problema de endian)
+    sxth  r1, r1 @adiciona sinal
 
     lsl   r3, r1, #10
     ldr   r2, =0x03FFFC00
